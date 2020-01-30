22309668d0d03c245f01f6d0b1f55657711696c3
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-30 12:00:41 +0000

    player has a weapon, and raises an error if tries to choose a weapon that isnt in the weapons array

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,9 @@
 class Player
+  attr_accessor :weapon
+  WEAPONS = [:rock, :paper, :scissors]
+
+  def choose_weapon(weapon)
+    fail "Not a possible weapon" unless WEAPONS.include? weapon
+    @weapon = weapon
+  end
 end
